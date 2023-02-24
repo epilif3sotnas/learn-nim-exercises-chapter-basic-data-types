7e7b3a01b409b2b6b65c448e071dacfb875aba09
Filipe Santos <filipesantos1999@hotmail.com>
2023-02-24 07:19:26 +0000

    Add exercise 5

---
+++ src/exercises/exercise_5.nim
@@ -0,0 +1,5 @@
+# Nim
+import std/strformat
+
+proc exercise_5*(first_name: string, last_name: string) =
+    echo(fmt("Full Name: {first_name} {last_name}"))
--- src/learn_nim_exercises_chapter_basic_data_types.nim
+++ src/learn_nim_exercises_chapter_basic_data_types.nim
@@ -1,4 +1,4 @@
-import exercises/[exercise_1, exercise_2, exercise_3, exercise_4]
+import exercises/[exercise_1, exercise_2, exercise_3, exercise_4, exercise_5]
 
 proc main() =
   # Exercise 1
@@ -12,6 +12,9 @@ proc main() =
 
   # Exercise 4
   exercise_4.exercise_4(3 / 8)
+
+  # Exercise 5
+  exercise_5.exercise_5("Filipe", "Santos")
 
 when is_main_module:
   main()
